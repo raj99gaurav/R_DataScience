9e955ffd20dd8252005e305864e8aa06490e2b9f
Raj Gaurav <49223793+raj99gaurav@users.noreply.github.com>
2023-04-11 17:43:57 +0530

    Add files via upload

---
+++ R Programming A-Z-Basics/Section 3 Fundamentals of R/Vectorized operations.R
@@ -0,0 +1,15 @@
+#Vectorized operations
+
+#to add two vectors in R we can simply add vectors using + sign without using loops
+#as everything is vectors so we can perform all type of operastions using boolean > < etc 
+
+#--- Recyling of vector ---
+what happens if we add small vector with big vector
+condition is small vector should be multiple of large vector in
+this case R will replicate small vector then add the two
+even it is not true the recyling will happen but we will get a warning as well
+
+
+#vectors can be arguments with in functions
+
+also vectors can be returned from the functions
