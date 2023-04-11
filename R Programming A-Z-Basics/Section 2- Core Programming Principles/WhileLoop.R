0583483709e73324690c211b0badac4a24a26035
Raj Gaurav <49223793+raj99gaurav@users.noreply.github.com>
2023-04-11 10:24:48 +0530

    Add files via upload

---
+++ R Programming A-Z-Basics/Section 2- Core Programming Principles/WhileLoop.R
@@ -0,0 +1,13 @@
+#Not widlely used as function like sapple lapply are there
+
+while (TRUE) {
+  print("Hello")
+}
+
+#ESC to exit loop
+
+counter <- 1
+while(counter < 12){
+  print(counter)
+  counter <- counter +1
+}
