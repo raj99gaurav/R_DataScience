7634fa755796a9af7cd806baa0d3f36cd6cb8a30
Raj Gaurav <49223793+raj99gaurav@users.noreply.github.com>
2023-04-11 10:06:41 +0530

    Add files via upload

---
+++ R Programming A-Z-Basics/Section 2- Core Programming Principles/LocalVariablesandOperators.R
@@ -0,0 +1,37 @@
+#Local:
+#TRUE T
+#FALSE F
+
+4 < 5
+
+10>100
+
+4 == 5
+
+# ==
+# !=
+# <
+# >
+# <=
+# >=
+# ! - not
+# | - or
+# & - and
+# isTRUE(x)
+
+result <- 4 < 5
+result
+
+typeof(result)
+
+result2 <- !TRUE
+result2
+
+result3 <- !(5>1)
+result3
+
+result | result2
+
+result & result2
+
+isTRUE(result)
