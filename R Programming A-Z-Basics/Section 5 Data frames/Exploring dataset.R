9217e0f6859a49695868952690861d0e3cdd90a4
Raj Gaurav <49223793+raj99gaurav@users.noreply.github.com>
2023-04-12 19:43:34 +0530

    Add files via upload

---
+++ R Programming A-Z-Basics/Section 5 Data frames/Exploring dataset.R
@@ -0,0 +1,21 @@
+stats
+#inko yaad krlo
+
+nrow(stats)
+#imported 195 rows -> always write these
+
+ncol(stats)
+#no of col 5
+
+head(stats) #top 6 rows 
+tail(stats) #bots 6 rows
+
+head(stats,10) 
+tail(stats,10)
+
+str(stats) #str stand for structure : deep breif of df
+
+#factor means that there are factor number of types there like high inome low income etc
+#can be used for a quick duplicate check
+#runif() - random uniform distrubution
+#rnorm() - random normal dis
