1086ab2cdf75a4f2a4ae758c96ad1d37d1fc9f3d
Raj Gaurav <49223793+raj99gaurav@users.noreply.github.com>
2023-04-12 11:58:10 +0530

    Add files via upload

---
+++ R Programming A-Z-Basics/Section 4 Matrices/Creating a function.R
@@ -0,0 +1,8 @@
+
+myplot <- function(data=Salary,rows=1:10){
+  Data <- data[rows,,drop=F]
+  matplot(t(Data),type="b",pch = 15:18,col=c(1:4,6))
+  legend("bottomleft",inset=0.01,legend=Players[1:3],col=c(1:4,6),pch = 15:18,horiz = F)
+}
+
+myplot(Salary,1:10)
