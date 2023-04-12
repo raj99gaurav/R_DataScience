f5d5cd649ea811240df9d12b7869fa73c00e6fc3
Raj Gaurav <49223793+raj99gaurav@users.noreply.github.com>
2023-04-12 10:00:25 +0530

    Add files via upload

---
+++ R Programming A-Z-Basics/Section 4 Matrices/Matirx operation on BB data.R
@@ -0,0 +1,12 @@
+Games
+rownames(Games)
+colnames(Games)
+
+Games["LeBronJames","2012"]
+
+FieldGoals
+
+FieldGoals / Games #shoul be of same size
+round(FieldGoals/Games,1)
+
+round(MinutesPlayed/Games)
