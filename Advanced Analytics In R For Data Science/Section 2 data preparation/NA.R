4d337b96b0e5f77dc6fef0dee88ea1918df6dc0d
Raj Gaurav <49223793+raj99gaurav@users.noreply.github.com>
2023-04-13 20:08:21 +0530

    Add files via upload

---
+++ Advanced Analytics In R For Data Science/Section 2 data preparation/NA.R
@@ -0,0 +1,8 @@
+#What is an NA? - missing values
+?NA
+
+#NA is a logical constant of length 1 which contains a missing value indicator
+
+TRUE #1
+FALSE #0
+NA #NA == TRUE/FALSE/NA/23/34/ETC ANSWER IS NA
