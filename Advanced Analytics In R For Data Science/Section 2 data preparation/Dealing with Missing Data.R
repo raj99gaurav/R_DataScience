f62e3884a4ea7aa4e318fbf0585e6d7d810b48d3
Raj Gaurav <49223793+raj99gaurav@users.noreply.github.com>
2023-04-13 20:05:10 +0530

    Add files via upload

---
+++ Advanced Analytics In R For Data Science/Section 2 data preparation/Dealing with Missing Data.R
@@ -0,0 +1,7 @@
+#Dealing with missing data : 
+1.Predict with 100% accuracy where you have two revenue and derive the third or you know for sure kya aayeaa
+2.Leave record as it is - if that field is not imp or the algo you apply can autofill it
+3.Remove record entirely - if you can restore an imp data drawback data becomes less
+4.Replace with mean or median - most pop (median is mostly used as it is less effected by outliers)
+5.fill in by exploring coorelations and sim
+6. intro dummy varriables for missingness
