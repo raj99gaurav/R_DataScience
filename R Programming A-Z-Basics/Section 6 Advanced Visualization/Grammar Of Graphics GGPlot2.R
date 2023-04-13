4c404450b2d2cbbd9fc2931f90f6df1a6491754d
Raj Gaurav <49223793+raj99gaurav@users.noreply.github.com>
2023-04-13 09:33:16 +0530

    Add files via upload

---
+++ R Programming A-Z-Basics/Section 6 Advanced Visualization/Grammar Of Graphics GGPlot2.R
@@ -0,0 +1,10 @@
+ #Charls minar made a map for france vs moscou in a 2d pic it wasnt grammer of graphics tho but he started the concept of layerting
+
+#Layers of a chart :
+1.Data #necessary - we cant see them mostly somewhere in background
+2.Aesthetics #necessary - we cant see them mapping of data to chart 
+3.Geometries #necessary - bars , circles , square
+4.Stats #additional data - distribution making a group etc or making a count of same data
+5.Facets #additional data - if we had multiple charts for same thing lets say over years
+6.Coordinates  #additional data - x axis and y axis also zoom in and out
+7.Theme  #additional data - everything else to please eyes so title legends ka size title ka size etc : non data ink bhi bolte h
