b18703fa14db0657e4d4360160c9bd9d33b34944
Raj Gaurav <49223793+raj99gaurav@users.noreply.github.com>
2023-04-14 14:36:51 +0530

    Add files via upload

---
+++ Advanced Analytics In R For Data Science/Section 4 Applied Family of Functions/Apply family.R
@@ -0,0 +1,12 @@
+#apply(M,1,mean) -> M is matrix , 1 = rows of matrix , mean - func we want to apply
+#apply(M,1,max)
+#we will get a vector with means of each row
+#2 -> col #we will get a vector with means of each col
+
+
+#The apply family has following func :
+#1.apply - use on matrix : either the row or col
+#2.lappy - apple a func. to ele of a list (L)
+#3.sapply - a version of lapply can simplify (S) the results so its not presented as a list
+
+#many more
