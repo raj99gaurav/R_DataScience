4097031339daa864eaeea386487b7b3970fa0c1a
Raj Gaurav <49223793+raj99gaurav@users.noreply.github.com>
2023-04-14 10:05:45 +0530

    Add files via upload

--- Advanced Analytics In R For Data Science/Section 3 Lists/Subsetting a list.R
+++ Advanced Analytics In R For Data Science/Section 3 Lists/Subsetting a list.R
@@ -1,1 +1,18 @@
 #Subsetting a list
+
+#challange : Access first date in unknownhours
+list_rl1$UnknownHours[1]
+
+
+#Subsetting a list
+list_rl1[1]
+list_rl1[1:2]
+list_rl1[c(1,4)] # 1 and 4
+
+sublist_rl1 <-list_rl1[c("Machine","Stats")]
+sublist_rl1
+
+sublist_rl1[[2]][2]
+sublist_rl1$Stats[2]
+
+#[[]] are not for subsetting but accessing list element
